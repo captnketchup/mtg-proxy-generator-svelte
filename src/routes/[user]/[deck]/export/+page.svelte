b3c06e44145631fd7f002ae382715c1720fdfffe
Daniel Kovari <74988054+captnketchup@users.noreply.github.com>
2025-04-10 17:30:41 +0200

    fix: export button downloads zip

--- src/routes/[user]/[deck]/export/+page.svelte
+++ src/routes/[user]/[deck]/export/+page.svelte
@@ -2,33 +2,10 @@
 	import * as Card from '$lib/components/ui/card';
 	import type { CardSimplified, DeckResponse } from '$lib/api/types';
 	import Button from '$lib/components/ui/button/button.svelte';
-	import JSZip from 'jszip';
 
 	const { data } = $props();
 
 	const cards = data.groupedCards;
-	const exportImages = data.exportImages;
-
-	function downloadZip() {
-		console.log('downloadZip called');
-		const zip = new JSZip();
-
-		exportImages.forEach((imageBuffer, index) => {
-			const blob = new Blob([imageBuffer], { type: 'image/png' });
-			zip.file(`image${index + 1}.png`, blob);
-		});
-
-		zip.generateAsync({ type: 'blob' }).then((zipBlob) => {
-			console.log('ZIP file created', zipBlob);
-
-			const link = document.createElement('a');
-			link.href = URL.createObjectURL(zipBlob);
-			link.download = 'images.zip';
-			document.body.appendChild(link);
-			link.click();
-			document.body.removeChild(link);
-		});
-	}
 </script>
 
 <div class="grid grid-cols-3 gap-4">
@@ -46,5 +23,7 @@
 </div>
 
 <div class="fixed bottom-0 right-0 p-4">
-	<Button onclick={() => downloadZip()} variant="default">Export</Button>
+	<a href={`/api/export/${data.deckId}`}>
+		<Button variant="default">Export</Button>
+	</a>
 </div>
